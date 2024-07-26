0ba979ace826e023f3774424ddba7211240bafaa
Bha-Gu <bhaumik050402@gmail.com>
2024-07-26 12:44:13 +0530

    Restructure binarization

--- lad/binarizer/cutpoint.py
+++ lad/binarizer/cutpoint.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class CutpointBinarizer:
-
+    # TODO implement Double Sided Binarisation
     def __init__(self, tolerance=0.0):
         self.__tolerance = tolerance
         self.__cutpoints = []
@@ -14,26 +14,28 @@ class CutpointBinarizer:
     def fit(self, X, y):
         self.__cutpoints = []
 
-        att = -1
-        for row in X.T:
-            att += 1
+        cc = X.shape[1]
+
+        for att in range(cc):
+            row = X[:, att]
             labels = None  # Previuos labels
-            u = -9999  # Previuos xi
+            u = None  # Previuos xi
 
             # Finding transitions
             for v in sorted(np.unique(row)):
-                variation = v - u  # Current - Previous
 
                 # Classes where v appears
                 indexes = np.where(row == v)[0]
                 __labels = set(y[indexes])
 
                 # Main condition
-                if labels is not None and variation > self.__tolerance:
+                if labels is not None:
+                    variation = v - u  # Current - Previous
+                    if variation > self.__tolerance:
 
-                    # Testing for transition
-                    if (len(labels) > 1 or len(__labels) > 1) or labels != __labels:
-                        self.__cutpoints.append((att, u + variation / 2.0))
+                        # Testing for transition
+                        if (len(labels) > 1 or len(__labels) > 1) or labels != __labels:
+                            self.__cutpoints.append((att, u + variation / 2.0))
 
                 labels = __labels
                 u = v
@@ -45,7 +47,7 @@ class CutpointBinarizer:
 
         for att, cutpoint in self.__cutpoints:
             # Binarizing
-            row = X.T[att]
+            row = X[:, att]
             row = row.reshape(X.shape[0], 1) <= cutpoint
             Xbin = np.hstack((Xbin, row))
 
