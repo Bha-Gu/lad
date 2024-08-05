068070d1eb3628bb582bf04a2caedf4ed0ea88b9
Bha-Gu <bhaumik050402@gmail.com>
2024-08-05 15:52:17 +0530

    Reimplement feature selection

--- lad/featureselection/greedy.py
+++ lad/featureselection/greedy.py
@@ -70,58 +70,138 @@ class GreedySetCover:
     def fit(self, Xbin, y):
         self.__selected = []
 
-        builder = UnWeightedSetCoveringProblem(
-            np.array([[] for _ in range(Xbin.shape[0])])
-        )
-        scp, invalid = builder.fit(Xbin, y)
+        Xbin_prune = Xbin.copy()
+        labels = np.unique(y)
+        subset_count = len(self.__selected)
+        class_count = len(labels)
+        sample_count = Xbin_prune.shape[0]
+        y_idx = []
+        for i in y:
+            y_idx.append(np.where(labels == i)[0][0])
+
+        subset_index = np.zeros(sample_count)
 
         invalids = []
 
-        effective_selected = scp
-        Xbin_prune = Xbin.copy()
+        while True:
+            feature_count = Xbin_prune.shape[1]
+            total = np.zeros((subset_count, class_count, feature_count), dtype=int)
+            y_t = np.zeros((subset_count, class_count), dtype=int)
 
-        while scp:
-            if scp is None:
+            for i in range(sample_count):
+                total[subset_index[i], y_idx[i]] += Xbin_prune[i]
+                y_t[subset_index[i], y_idx[i]] += 1
+
+            T = np.sum(total, axis=1)
+            YT = np.sum(y_t, axis=1)
+
+            final = np.zeros((subset_count, feature_count), dtype=int)
+            for feature in range(feature_count):
+                for subset in range(subset_count):
+                    final[subset, feature] += YT[subset] * T[subset, feature]
+                    for classs in range(class_count):
+                        final[subset, feature] -= (
+                            y_t[subset, classs] * total[subset, classs, feature]
+                        )
+                        for subclass in range(classs + 1, class_count):
+                            final[subset, feature] -= (
+                                2
+                                * total[subset, classs, feature]
+                                * total[subset, subclass, feature]
+                            )
+
+            final_rank = np.sum(final, axis=0)
+            if np.sum(final_rank) == 0:
                 break
-            mask = np.ones(Xbin_prune.shape[1], dtype=bool)
-            mask[invalid] = False
-            print(mask, invalid)
-            mask[effective_selected] = False
-            invalid = np.where(mask == False)  # noqa: E712
-            invalid = invalid[0]
-            Xbin_prune = Xbin_prune[:, mask]
-            actual_next_feature = scp
-            for i in invalids:
-                if i < actual_next_feature:
-                    actual_next_feature += 1
-                for j in range(len(invalid)):
-                    if i < invalid[j]:
-                        invalid[j] += 1
-            print(invalids, invalid)
-            for i in invalid:
-                if i not in invalids:
-                    invalids.append(i)
-            invalids.sort()
+            best = np.argmax(final_rank)
 
-            self.__selected.append(actual_next_feature)
+            self.__selected.append(best)
             if len(self.__selected) == self.__max:
                 break
-
             self.__selected.sort()
 
-            selected_list = Xbin[:, self.__selected]
-            effective_selected = scp
+            for i in range(sample_count):
+                if Xbin_prune[i, best]:
+                    subset_index += 2 ** (len(self.__selected) - 1)
 
-            for j in invalids:
-                if scp >= j:
-                    effective_selected -= 1
-            print(scp)
-            print(self.__selected)
-            print(effective_selected)
-            builder = UnWeightedSetCoveringProblem(selected_list)
-            scp, invalid = builder.fit(Xbin_prune, y)
+            rejected = np.where(final_rank == 0)[0]
+            base_best = best
+            base_rejected = rejected.copy()
 
-        self.__selected.sort()
+            for invalid in invalids:
+                if invalid < base_best:
+                    base_best += 1
+                for i in range(len(rejected)):
+                    if invalid < base_rejected[i]:
+                        base_rejected[i] += 1
+
+            invalids.append(base_best)
+            for r in base_rejected:
+                if r not in invalids:
+                    invalids.append(r)
+                else:
+                    print("Debug print, should be impossible")
+            invalids.sort()
+
+            mask = np.ones(feature_count, dtype=bool)
+            mask[rejected] = False
+            mask[best] = False
+
+            Xbin_prune = Xbin_prune[:, mask]
+
+        # builder = UnWeightedSetCoveringProblem(
+        #     np.array([[] for _ in range(Xbin.shape[0])])
+        # )
+        # scp, invalid = builder.fit(Xbin, y)
+        #
+        # invalids = []
+        #
+        # effective_selected = scp
+        # Xbin_prune = Xbin.copy()
+        #
+        # while scp:
+        #     if scp is None:
+        #         break
+        #     mask = np.ones(Xbin_prune.shape[1], dtype=bool)
+        #     mask[invalid] = False
+        #     print(mask, invalid)
+        #     mask[effective_selected] = False
+        #     invalid = np.where(mask == False)  # noqa: E712
+        #     invalid = invalid[0]
+        #     Xbin_prune = Xbin_prune[:, mask]
+        #     actual_next_feature = scp
+        #     for i in invalids:
+        #         if i < actual_next_feature:
+        #             actual_next_feature += 1
+        #         for j in range(len(invalid)):
+        #             if i < invalid[j]:
+        #                 invalid[j] += 1
+        #     print(invalids, invalid)
+        #     for i in invalid:
+        #         if i not in invalids:
+        #             invalids.append(i)
+        #     invalids.sort()
+        #
+        #     self.__selected.append(actual_next_feature)
+        #     if len(self.__selected) == self.__max:
+        #         break
+        #
+        #     self.__selected.sort()
+        #
+        #     selected_list = Xbin[:, self.__selected]
+        #     effective_selected = scp
+        #
+        #     for j in invalids:
+        #         if scp >= j:
+        #             effective_selected -= 1
+        #     print(scp)
+        #     print(self.__selected)
+        #     print(effective_selected)
+        #     builder = UnWeightedSetCoveringProblem(selected_list)
+        #     scp, invalid = builder.fit(Xbin_prune, y)
+        #
+        # self.__selected.sort()
+        #
 
     def transform(self, Xbin):
         return Xbin[:, self.__selected]
