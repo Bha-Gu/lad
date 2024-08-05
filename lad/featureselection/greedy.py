bf276d06baa99df2a22d504a5950078a2b8ae59a
Bha-Gu <bhaumik050402@gmail.com>
2024-08-05 10:47:11 +0530

    debug

--- lad/featureselection/greedy.py
+++ lad/featureselection/greedy.py
@@ -113,7 +113,7 @@ class GreedySetCover:
                     if self.__selected[i] >= invalids[j]:
                         effective_selected[i] -= 1
             print(scp)
-
+            print(invalids)
             print(self.__selected)
             builder = UnWeightedSetCoveringProblem(selected_list)
             scp, invalid = builder.fit(Xbin_prune, y)
