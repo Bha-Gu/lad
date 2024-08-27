633b5d4baf8ecc3a492b9d199c7829e7e624c950
Bha-Gu <bhaumik050402@gmail.com>
2024-08-27 10:47:44 +0530

    debug

--- lad/featureselection/greedy.py
+++ lad/featureselection/greedy.py
@@ -74,7 +74,10 @@ class GreedySetCover:
                 if a.all() or b.all():
                     removed.append(j)
 
-        self.selected = [f for i, f in enumerate(features) if f not in removed]
+        self.selected = [f for f in features if f not in removed]
+        print(self.selected)
+        print(features)
+        print(removed)
 
     def transform(self, Xbin):
         return Xbin.select(self.__selected)
