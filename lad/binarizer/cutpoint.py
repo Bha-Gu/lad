9c17caa368196d774e0b9a57a475feb9c7937527
Bha-Gu <bhaumik050402@gmail.com>
2024-09-05 10:37:35 +0530

    update bin size tolerance calculations

--- lad/binarizer/cutpoint.py
+++ lad/binarizer/cutpoint.py
@@ -76,7 +76,9 @@ class CutpointBinarizer:
             col_y: pl.DataFrame = pl.DataFrame([X[feature], y])
             if schema[feature].is_numeric():
                 sorted_values: pl.DataFrame = col_y.sort(feature)
-                delta: float = sorted_values[feature].diff(null_behavior="drop").sum()
+                delta: float = (
+                    sorted_values[feature].diff(null_behavior="drop") ** 2
+                ).sum() ** 0.5
                 tolerance: float = delta / (len(sorted_values) - 1)
                 cutpoints: set[float] = set()
                 prev_cutpoint: float | None = None
