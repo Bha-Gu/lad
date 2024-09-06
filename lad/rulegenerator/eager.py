072e02b2e49bbc8b26196140bb5471a8fc226390
Bha-Gu <bhaumik050402@gmail.com>
2024-09-06 13:18:46 +0530

    debug

--- lad/rulegenerator/eager.py
+++ lad/rulegenerator/eager.py
@@ -135,7 +135,7 @@ class MaxPatterns:
                     pos_len = len(X_pos)
                     FP = len(X_neg.filter(filter))
                     neg_len = len(X_neg)
-                    recall = TP + p_s / (TP + FP + p_s + n_s)
+                    recall = TP + p_s / (pos_shape)
                     if self.__base_recall <= recall and pos_len > 0.0:
                         print(recall)
                         precision = TP / pos_len
